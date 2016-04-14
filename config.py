7815fba2aa4935c41f181f70588630a9e9843e43
andre yanpolsky <andrey.yanis@gmail.com>
2016-04-13 23:09:50 -0400

    gets some json

---
+++ config.py
@@ -0,0 +1,8 @@
+class BarzerSettings(object):
+    # available instances of Barzer
+    BARZER_INSTANCES = {
+        'default' : {
+            'url': 'https://my.barzer.net/query/json',
+            'key': 'yTIzlFaf02QBBa3wRNPD2xjeI0R1P1hPHwL3dHtv'
+        }
+    }

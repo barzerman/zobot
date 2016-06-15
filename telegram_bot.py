f31f33ed4897ce6962884bd37ee8df79cb5a0b67
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-06-15 08:19:10 +0300

    incoplete changes

--- telegram_bot.py
+++ telegram_bot.py
@@ -18,7 +18,7 @@ protocols_msg = '/n'.join(['/' + x for x in available_protocols])
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     zobot.reset()
-    bot.reply_to(message, "Hi! What protocol do you want to use?\n" + protocols_msg)
+    bot.send_message(message.chat.id, "Hi! What protocol do you want to use?\n" + protocols_msg)
 
 
 @bot.message_handler(commands=available_protocols)
@@ -26,16 +26,16 @@ def init_protocol(message):
     print message.text
     try:
         zobot.init_from_external(message.text.split('/')[-1], 'telegram:' + str(message.chat.id))
-        bot.reply_to(message, zobot.say())
+        bot.send_message(message.chat.id, zobot.say())
     except:
-        bot.reply_to(message, 'Can not initialize protocol')
+        bot.send_message(message.chat.id, 'Can not initialize protocol')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     if zobot.token:
-        bot.reply_to(message, zobot.say(message.text))
+        bot.send_message(message.chat.id, zobot.say(message.text))
     else:
-        bot.reply_to(message, 'No protocol is selected. Please select one of the protocols:\n' + protocols_msg)
+        bot.send_message(message.chat.id, 'No protocol is selected. Please select one of the protocols:\n' + protocols_msg)
 
 bot.polling()

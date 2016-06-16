b6ce361c56df4c69d94db8014490c32bc811987f
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-06-16 09:28:32 +0300

    deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ssh -i "./scripts/botx.pem" ubuntu@ec2-52-34-35-240.us-west-2.compute.amazonaws.com << EOF
-cd ~/zobot && git pull origin && ~/zobot/scripts/stop_bot.sh && ~/zobot/scripts/start_bot.sh
+cd ~/zobot && git pull origin && source venv/bin/activate && pip install -r requirements.txt && echo "`whoami`" &&  (exec "./scripts/stop_bot.sh") && (exec "./scripts/start_bot.sh") && echo '[OK]'
 EOF
